bc3af88e1f07765b3ee22bccd34cb3585a007b9f
githuubb420 <126564609+githuubb420@users.noreply.github.com>
2023-04-23 06:09:47 -1100

    Delete main directory

--- main/plug/start.py
+++
@@ -1,7 +0,0 @@
-from telethon import events
-from main import bot
-
-
-@bot.register(events.NewMessage(pattern='/start'))
-async def start_handler(event):
-    await event.bot.send_message('Bot has been started')
